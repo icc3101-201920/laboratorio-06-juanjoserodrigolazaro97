65761278a279369f8bc38edd5b09238d5fccd087
juanjoserodrigolazaro97 <jjrodrigo@miuandes.cl>
2019-09-27 09:20:35 -0300

    second commit

---
+++ Laboratorio_5_OOP_201902/Visualization.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Laboratorio_5_OOP_201902.Cards;
+
+namespace Laboratorio_5_OOP_201902
+{
+    public static class Visualization
+    {
+        public static void ShowHand(Hand hand) {
+
+        }
+
+        public static void ShowDecks(List<Deck>decks)
+        {
+
+        }
+
+        public static void ShowCaptains(List<SpecialCard> captains)
+        {
+
+        }
+
+        public static void GetUserInput(int maxInput, bool stopper = false)
+        {
+
+        }
+       
+        public static void ConsoleError(string message)
+        {
+
+        }
+        public static void ShowProgramMessage(string message)
+        {
+
+        }
+        public static void ShowListOptions(List<string> options, string message = null)
+        {
+
+        }
+        public static void ClearConsole()
+        {
+
+        }
+        
+            
+            
+    }
+}
